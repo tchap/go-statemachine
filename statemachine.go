6056d6ed9618ee263d4145963d9d5f260cbcda59
Ondrej Kupka <ondra.cap@gmail.com>
2013-08-17 20:01:38 +0200

    Add some documentation
    Signed-off-by: Ondrej Kupka <ondra.cap@gmail.com>

--- statemachine.go
+++ statemachine.go
@@ -33,26 +33,48 @@ type (
 	Context   interface{}
 )
 
+// Events are the basic units that can be processed by a state machine.
 type Event struct {
 	Type EventType
 	Data interface{}
 }
 
+// Various EventHandlers can be registered to process events in particular states.
+// By registering event handlers we build up a mapping of state x event -> handler
+// and the handler is invoked exactly in the defined state when the defined event
+// is emitted.
+//
+// Once a handler is invoked, its role is to take the StateMachine into the next
+// state, doing some useful work on the way. There is a context variable that
+// is to be used to keep come useful data between handler calls. So, the state
+// is for consistency, the context is for keeping data.
+//
+// If an event is emitted in a state where no handler is defined,
+// ErrIllegalEvent is returned.
 type EventHandler func(state State, ctx Context, evt *Event) (next State)
 
+// StateMachine is the only struct this package exports. Once an event is
+// emitted on a StateMachine, the relevant handler is fetched and invoked.
+// StateMachine takes care of all the synchronization, it is thread-safe.
+// It does not use any locking, just channels. While that may be a bit more
+// overhead, it is more robust and clear.
 type StateMachine struct {
 	state State
 	ctx   Context
 
 	handlers [][]EventHandler
 
-	cmdCh        chan *command
-	terminatedCh chan struct{}
+	cmdCh        chan *command // Send commands to the background loop
+	terminatedCh chan struct{} // Signal that the state machine is terminated
 }
 
 // CONSTRUCTOR ----------------------------------------------------------------
 
+// Create new StateMachine. Allocate internal memory for particular number of
+// states and events, set internal channel size. As long as the internal channel
+// is not full, most of the methods are non-blocking.
 func NewStateMachine(initState State, initCtx Context, stateCount, eventCount, mailboxSize uint) *StateMachine {
+	// Allocate enough space for the handlers.
 	table := make([][]EventHandler, stateCount)
 	for i := range table {
 		table[i] = make([]EventHandler, eventCount)
@@ -66,6 +88,7 @@ func NewStateMachine(initState State, initCtx Context, stateCount, eventCount, m
 		terminatedCh: make(chan struct{}),
 	}
 
+	// Start background goroutine.
 	go sm.loop()
 
 	return &sm
@@ -94,6 +117,8 @@ type cmdOnArgs struct {
 	h EventHandler
 }
 
+// Register an event handler. Only one handler can be set per state and event.
+// It is non-blocking as long as the internal channel is not full.
 func (sm *StateMachine) On(t EventType, s State, h EventHandler) error {
 	return sm.send(&command{
 		cmdOn,
@@ -109,6 +134,8 @@ type cmdIsHandlerDefinedArgs struct {
 	ch chan bool
 }
 
+// Check if a handler is defined for this state and event.
+// It is non-blocking as long as the internal channel is not full.
 func (sm *StateMachine) IsHandlerDefined(s State, t EventType) (defined bool, err error) {
 	replyCh := make(chan bool, 1)
 	err = sm.send(&command{
@@ -129,6 +156,8 @@ type cmdOffArgs struct {
 	t EventType
 }
 
+// Drop a handler assigned to the state and event.
+// It is non-blocking as long as the internal channel is not full.
 func (sm *StateMachine) Off(s State, t EventType) error {
 	return sm.send(&command{
 		cmdOff,
@@ -143,6 +172,9 @@ type cmdEmitArgs struct {
 	ch chan error
 }
 
+// Emit a new event. It is possible to pass a channel to the internal loop
+// to check if the handler was found and scheduled for execution.
+// It is non-blocking as long as the internal channel is not full.
 func (sm *StateMachine) Emit(event *Event, replyCh chan error) error {
 	return sm.send(&command{
 		cmdEmit,
@@ -152,6 +184,10 @@ func (sm *StateMachine) Emit(event *Event, replyCh chan error) error {
 
 // Terminate ------------------------------------------------------------------
 
+// Terminate the internal event loop and close all internal channels.
+// Particularly the termination channel is closed to signal all producers that
+// they can no longer emit any events and shall exit.
+// It is non-blocking as long as the internal channel is not full.
 func (sm *StateMachine) Terminate() error {
 	return sm.send(&command{
 		cmdTerminate,
@@ -159,12 +195,19 @@ func (sm *StateMachine) Terminate() error {
 	})
 }
 
+// TerminateChannel can be used to obtain a channel that is closed once
+// the state machine is terminated and is no longer willing to accept any events.
+// This is useful if you want to start multiple goroutines to asynchronously
+// post events. You can just start them, pass them this termination channel
+// and leave them be. The only requirement is that those producer goroutines
+// should exit or simply stop posting any events as soon as the channel is closed.
 func (sm *StateMachine) TerminatedChannel() chan struct{} {
 	return sm.terminatedCh
 }
 
 // INTERNALS ------------------------------------------------------------------
 
+// Helper method for sending events to the internal event loop.
 func (sm *StateMachine) send(cmd *command) error {
 	select {
 	case sm.cmdCh <- cmd:
@@ -174,6 +217,8 @@ func (sm *StateMachine) send(cmd *command) error {
 	}
 }
 
+// The internal event loop processes events (commands) passed to it in
+// a sequential manner.
 func (sm *StateMachine) loop() {
 	for {
 		cmd := <-sm.cmdCh
@@ -214,6 +259,11 @@ func (sm *StateMachine) loop() {
 // ERRORS ---------------------------------------------------------------------
 
 var (
+
+	// Returned from Emit if there is no mapping for the current state and the
+	// event that is being emitted.
 	ErrIllegalEvent = errors.New("Illegal event received")
+
+	// Returned from a method if the state machine is already terminated.
 	ErrTerminated   = errors.New("State machine terminated")
 )
